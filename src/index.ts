bb94f7680d4d3e5e72bd933c5c08e8ef5c29db08
Stanislav Bereza <stasbereza@Stanislavs-MacBook-Pro.local>
2020-12-24 01:59:38 +0200

    add project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { UserForm } from './views/UserForm';
+import { User } from './models/User';
+
+const user = User.buildUser({ name: 'Stanislav', age: 37 });
+
+const root = document.getElementById('root');
+
+if (root) {
+  const userForm = new UserForm(root, user);
+
+  userForm.render();
+} else {
+  throw new Error('Root element not found!');
+}
---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
+import { Collection } from './Collection';
+
+export interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+const rootUrl = 'http://localhost:3000/users';
+
+export class User extends Model<UserProps> {
+  static buildUser(attr: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attr),
+      new ApiSync<UserProps>(rootUrl),
+      new Eventing()
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
+    );
+  }
+
+  setRandomAge(): void {
+    const age = Math.round(Math.random() * 100);
+    this.set({ age });
+  }
+}
