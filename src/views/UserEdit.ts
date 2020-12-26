30ef3bfa7f6454fcd00a17ef491f0e780000dff0
Stanislav Bereza <stasbereza@Stanislavs-MacBook-Pro.local>
2020-12-26 15:53:59 +0200

    implement nesting and mapping regions

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,7 +1,24 @@
 import { View } from '../views/View';
 import { User, UserProps } from '../models/User';
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
 
 export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return {
+      UserShow: '.user-show',
+      UserForm: '.user-form',
+    };
+  }
+
+  onRender(): void {
+    const userShow = new UserShow(this.regions.UserShow, this.model);
+    const userForm = new UserForm(this.regions.UserForm, this.model);
+
+    userShow.render();
+    userForm.render();
+  }
+
   template(): string {
     return `
         <div>
