ea48191bf0e98098d2aa1df3da46b4e80583efc9
Stanislav Bereza <stasbereza@Stanislavs-MacBook-Pro.local>
2020-12-26 12:14:03 +0200

    add UserEdit and UserShow classes, change ts config

--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -12,7 +12,7 @@ export class Eventing {
   trigger = (eventName: string): void => {
     const handlers = this.events[eventName];
 
-    if (!handlers && handlers.length === 0) {
+    if (!handlers || handlers.length === 0) {
       return;
     }
 
